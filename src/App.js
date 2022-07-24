d0bc57d13ffdeb80279f5ca3f2f28893eca289db
Kunkanya <kunkanyaremmer@hotmail.com>
2022-07-24 16:05:24 +0200

    V2: add customized style to modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Modal from "./lib/components/Modal/Modal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react'
+
+const App = () => {
+    const [isShow, setIsShow] = useState(false)
+    const showModal = () => {
+        setIsShow(true)
+    }
+    return (
+        <div>
+            <button onClick={showModal}>
+                ShowModal
+            </button>
+            {isShow ? 
+                <Modal
+                show={isShow}
+                onCloseFunction={() => { setIsShow(false) }}
+                modalHeaderContent={"Modal Header!"}
+                modalBodyContent="Succesfully created new employee!!!" 
+                buttonContent="Close"
+                buttonOnclickFn={()=>{ setIsShow(false)}}
+                />
+                : ""
+            }
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/lib/components/Modal/Modal.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../Modal/Modal.css'
+
+/**
+ * 
+ * @param {Boolean} show
+ * @param {funcion} onCloseFunction
+ * @param {object, string} styleModalWrapper //Style for Inline react CSS can be string or set of style objects.
+ * @param {object, string} styleModalContainer //Style for Inline react CSS can be string or set of style objects.
+ * @param {object, string} styleModalButton //Style for Inline react CSS can be string or set of style objects.
+ * @param {node} modalHeaderContent //Anything that can be rendered: numbers, strings, elements or an array
+ * @param {node} modalBodyContent //Anything that can be rendered: numbers, strings, elements or an array
+ * @param {string} buttonContent 
+ * @returns HTMLElement
+ */
+
+const Modal = ({
+    show,
+    onCloseFunction,
+
+    styleModalWrapper,
+    styleModalContainer,
+    styleButton,
+
+    modalHeaderContent,
+    modalBodyContent,
+
+    buttonContent
+
+}) => {
+
+    return (
+        <>
+            {show ?
+                <div style={styleModalWrapper}
+                    className="modalWrapper"
+                    onClick={onCloseFunction} >
+                    <div className='modalContainer'
+                        style={styleModalContainer}
+                        onClick={(e) => e.stopPropagation()}>
+                        <section className='modalHeader'>
+                            {modalHeaderContent}
+                        </section>
+                        <section className='modalBody'>
+                            {modalBodyContent}
+                        </section>
+                        <section className='modalFooter'>
+
+                            <button style={styleButton}
+                                className="button"
+                                onClick={onCloseFunction}>
+                                {buttonContent}
+                            </button>
+                        </section>
+                    </div>
+                </div>
+                : ""
+            }
+        </>
+    )
+}
+
+Modal.propTypes = {
+    show: PropTypes.bool.isRequired,
+    onCloseFunction: PropTypes.func.isRequired,
+
+    styleModalWrapper : PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.object
+    ]),
+    styleModalContainer : PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+]),
+    styleButton:  PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+]),
+
+    modalHeaderContent:PropTypes.node,
+    modalBodyContent: PropTypes.node,
+
+    buttonContent: PropTypes.string
+}
+
+export default Modal
